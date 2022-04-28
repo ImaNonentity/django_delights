471df348924566741ee27233346ae61310c6b910
ImaNonentity <childmoon11@gmail.com>
2022-04-28 19:33:31 +0300

    postgres settings

--- djangodelights/settings.py
+++ djangodelights/settings.py
@@ -81,9 +81,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'root',
-        'USER': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
